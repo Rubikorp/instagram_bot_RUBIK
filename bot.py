657e00793876f1fcda84e25fc8f7f42584feb093
Rubikorp <96957118+Rubikorp@users.noreply.github.com>
2022-02-10 15:08:32 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+from instamn import InstaLOGIN
+from pathlib import Path
+import time
+import random
+
+l = []
+with open('users.txt') as f:
+    l = f.read().splitlines()
+print(l)
+
+dat = []
+with open('data.txt') as f:
+    dat = f.read().splitlines()
+print(dat)
+
+
+if __name__ == '__main__':
+    # Auto login
+    insta = InstaLOGIN(username='login', password='pass', headless=False)
+
+    i = 0
+    while i<len(l):
+        insta.sendMessage(user=l[i], message=dat )
+        i+=1
+        if i == i % 20 == 0:
+            time.sleep(1200)
+            print('достигнут лимит в час, пауза 30 минут')
+
